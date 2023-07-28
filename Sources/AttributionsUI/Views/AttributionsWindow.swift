fd0f11211842186a34bc11bd774cd0f93452b040
Martin Johannesson <mjohannesson@gmail.com>
2023-07-28 16:51:29 +0200

    Add header parameter to AttributionsWindow

--- Sources/AttributionsUI/Views/AttributionsWindow.swift
+++ Sources/AttributionsUI/Views/AttributionsWindow.swift
@@ -8,19 +8,22 @@ public struct AttributionsWindow: Scene {
     
     private let entries: [Attributions.Entry]
     
-    public init(_ entries: [Attributions.Entry]) {
+    private let header: String?
+    
+    public init(_ entries: [Attributions.Entry], header: String? = nil) {
         self.entries = entries
+        self.header = header
     }
         
     public var body: some Scene {
         Window("Attributions", id: Self.windowID) {
             Attributions(entries)
+                .attributionsHeader(header)
                 .frame(minWidth: 500, minHeight: 300)
         }
         .defaultPosition(.center)
         .defaultSize(width: 500, height: 300)
         .windowResizability(.contentMinSize)
-        .windowStyle(.hiddenTitleBar)
     }
 }
 
